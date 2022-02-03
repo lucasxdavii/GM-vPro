a2d5ee9e0bc7742888ad73109fdaae7b3fadcac2
Draw099 <87025167+Draw099@users.noreply.github.com>
2022-02-03 10:08:47 -0300

    Add files via upload

---
+++ src/zDraw/Main.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace zDraw;
+
+use pocketmine\Server;
+use pocketmine\Player;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+    
+    public function onEnable(){
+        
+    }
+    
+    public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
+        switch($cmd->getName()){
+            case "gm":
+                if($sender->hasPermission("gm.use")){
+                    if(isset($args[0])){
+                        switch(strtolower($args[0])){
+                            case "0":
+                                $sender->getPlayer()->setGamemode(0);
+                                $sender->sendMessage("§aModo de jogo alterado para Survival");
+                            break;
+                            
+                            case "1":
+                                $sender->getPlayer()->setGamemode(1);
+                                $sender->sendMessage("§aModo de jogo alterado para Creativo");
+                            break;
+                            
+                            case "2":
+                                $sender->getPlayer()->setGamemode(2);
+                                $sender->sendMessage("§aModo de jogo alterado para Aventura");
+                            break;
+                            
+                            case "3":
+                                $sender->getPlayer()->setGamemode(3);
+                                $sender->sendMessage("§aModo de jogo alterado para Espectador");
+                            break;
+                        }
+                    } else {
+                        $sender->sendMessage("§cUtilize /gm <0, 1, 2, 3>");
+                    }
+                } else {
+                    $sender->sendMessage("Voce nao tem permisao para usar!");
+                }
+                
+        }
+      return true;
+    }
+}
